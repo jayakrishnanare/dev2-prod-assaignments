1b3dff8d3ea1bc981f30682e99c9c569a681c7dd
jayakrishna <88233543+naresriv@users.noreply.github.com>
2023-08-01 15:46:44 +0530

    new commit

--- obj1.js
+++ obj1.js
@@ -1,1 +1,35 @@
-console.log("hii good")
+let obj = {};
+
+obj.name = "jayakrishna";
+obj.age = 22;
+obj.location = "hyderabad"
+
+
+//checking th objet has keys or not
+
+let result = obj.hasOwnProperty("name");
+// console.log(result);
+let data = obj.valueOf();
+// console.log(data)
+
+
+const newObject = new Object();
+newObject.name = "jayakrishna";
+newObject.age = 22;
+// console.log(newObject.valueOf())
+
+const object = new Object(1);
+// console.log(typeof object)
+
+//constructor property
+
+let o1 = {};
+let output = o1.constructor  === Object;
+// console.log(output)
+
+let o2 = new Object();
+output = o2.constructor === Object;
+// console.log(output)
+
+
+
