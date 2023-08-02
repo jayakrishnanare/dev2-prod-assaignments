60e46318da076d38d450214851f3b93f379015a8
jayakrishna <88233543+naresriv@users.noreply.github.com>
2023-08-02 12:35:16 +0530

    new changes

--- obj-defineProperty.js
+++ obj-defineProperty.js
@@ -17,3 +17,15 @@ Object.defineProperty(obj1,'property2',{
 
 obj1.property2 = "good aternoon";
 console.log(obj1.property2)
+
+
+//
+
+const obj2 = {};
+
+Object.defineProperty(obj2,'property',{
+    value: "bhargav thakkelapati",
+    writable:true
+});
+
+console.log(obj2.property)
